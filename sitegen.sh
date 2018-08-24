8d2a72ae893542a65b0e1f102cca9bcd0da0a79a
Sagar Gubbi <sagar.writeme@gmail.com>
2018-08-24 21:00:16 +0200

    Added One Piece 912

---
+++ sitegen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find one-piece -mindepth 1 -maxdepth 1 -type d -exec sh -c "python pagerender.py {}/index.md blog.html > {}/index.html" \;
+find one-piece -mindepth 1 -maxdepth 1 -type d -exec sh -c "python pagerender.py {}/index.md blogamp.html > {}/amp/index.html" \;
+
